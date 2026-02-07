fedb9173ad5a98fd29d3aafc3dd7f8424a84ef1e
yazid138 <ma39isy@gmail.com>
2026-02-07 20:43:10 +0700

    feat: Initialize Next.js application with Material-UI, Tailwind CSS, and custom fonts, along with adding `@emotion/cache` dependency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
